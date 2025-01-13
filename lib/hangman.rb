284b87e5d14151ed2d1ed4f6c68edb79bff750bc
Joel Paul <joel.paul@student.unsw.edu.au>
2025-01-13 16:28:03 +1100

    IN PROGRESS: Implement saving and loading

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,69 +1,141 @@
 require 'set'
 
-MIN_LEN = 5
-MAX_LEN = 12
 
-MAX_WRONG = 8
+class Hangman
+  def initialize(min_len=5, max_len=12, max_wrong=8)
+    @min_len = min_len
+    @max_len = max_len
+    @max_wrong = max_wrong
 
-def display_guess(secret, guessed)
-  puts secret.chars.map { |c| guessed.include?(c) ? c : '_' }.join(' ')
-end
-
-def get_incorrect(secret, guessed)
-  (guessed.to_a - secret.chars).sort
-end
-
-def display_incorrect(secret, guessed)
-  incorrect = get_incorrect(secret, guessed)
-  puts "Incorrect guesses (#{incorrect.length}/#{MAX_WRONG}): #{incorrect.join(', ')}"
-end
-
-def display(secret, guessed)
-  puts ''
-  display_guess(secret, guessed)
-  display_incorrect(secret, guessed)
-end
-
-def game_over?(secret, guessed)
-  incorrect = get_incorrect(secret, guessed)
-  secret.chars.all?{ |c| guessed.include? c} or incorrect.length == MAX_WRONG
-end
-
-words = File.readlines('google-10000-english-no-swears.txt').map { |word| word.chomp }.select do |word|
-  word.length >= MIN_LEN and word.length <= MAX_LEN
-end
-
-puts 'Let\'s play Hangman! Try figure out the word, or type `quit` to exit.'
-secret = words.sample
-# puts secret
-
-guessed = Set.new
-display(secret, guessed)
-
-until game_over?(secret, guessed)
-  print 'Guess a letter: '
-  guess = gets.chomp
-  if guess == 'quit'
-    return
+    @secret = ''
+    @guessed = Set.new
   end
 
-  until guess.length == 1 and guess.match(/^[[:alpha:]]+$/)
-    puts 'Invalid option!'
-    display(secret, guessed)
 
-    print 'Guess a letter: '
-    guess = gets.chomp
-    if guess == 'quit'
+  def main_menu
+    puts 'Welcome to Hangman!'
+    puts '1. New game'
+    puts '2. Load game'
+    puts '3. Quit'
+
+    print 'Chose an option: '
+    choice = gets.chomp
+    until choice_valid?(choice, 3)
+      print 'Choice must be between 1 and 3: '
+      choice = gets.chomp
+    end
+
+    puts
+    case choice
+    when '1'
+      new_game
+    when '2'
+      load_menu
+    else
       return
     end
   end
-  guessed << guess.downcase
-  display(secret, guessed)
+
+
+  def new_game
+    @secret = rand_word
+    puts 'Let\'s play Hangman!'
+    game_loop
+    display_results
+  end
+
+
+  def load_menu
+    saves = Dir.exist?('saves') ? Dir.entries('saves') - ['..', '.'] : []
+    if saves.empty?
+      puts 'No saves found, returning to main menu...'
+      return
+    else
+      saves.each_with_index do |val, index|
+        puts "#{index+1}: #{val}"
+      end
+    end
+  end
+
+
+  private
+
+
+  def rand_word
+    words = File.readlines('google-10000-english-no-swears.txt').map { |word| word.chomp }.select do |word|
+      word.length >= @min_len and word.length <= @max_len
+    end
+    words.sample
+  end
+
+
+  def get_incorrect
+    (@guessed.to_a - @secret.chars).sort
+  end
+  
+
+  def display_guess
+    puts @secret.chars.map { |c| @guessed.include?(c) ? c : '_' }.join(' ')
+  end
+
+
+  def display_incorrect
+    incorrect = get_incorrect
+    puts "Incorrect guesses (#{incorrect.length}/#{@max_wrong}): #{incorrect.join(', ')}"
+  end
+
+
+  def display_results
+    if @secret.chars.all?{ |c| @guessed.include? c}
+      puts 'You wins! :D'
+    else
+      puts 'You loses :('
+      puts "The word was `#{@secret}`"
+    end
+  end
+
+
+  def guess_valid?(input)
+    input.length == 1 and input.match(/^[[:alpha:]]+$/)
+  end
+
+  def choice_valid?(input, max_index)
+    int = Integer(input) rescue -1
+    int.between?(1, max_index)
+  end
+
+
+  def game_loop
+    display_game
+    until game_over?
+      print 'Guess a letter: '
+      guess = gets.chomp
+    
+      until guess_valid?(guess)
+        puts 'Invalid letter!'
+        display_game
+    
+        print 'Guess a letter: '
+        guess = gets.chomp
+      end
+      @guessed << guess.downcase
+      display_game
+    end
+  end
+
+  def display_game
+    puts ''
+    display_guess
+    display_incorrect
+  end
+
+
+  def game_over?
+    incorrect = get_incorrect
+    @secret.chars.all?{ |c| @guessed.include? c} or incorrect.length == @max_wrong
+  end
 end
 
-if secret.chars.all?{ |c| guessed.include? c}
-  puts 'You wins! :D'
-else
-  puts 'You loses :('
-  puts "The word was `#{secret}`"
-end
+
+hangman = Hangman.new
+hangman.main_menu
