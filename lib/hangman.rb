1ad11b7a1478f6c904ae00290cd9fa116b9b72df
Joel Paul <joel.paul@student.unsw.edu.au>
2025-01-15 14:51:17 +1100

    Implement quitting

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -2,6 +2,12 @@ require 'set'
 
 
 class Hangman
+  @@comands = {
+    help: 'Displays the list of commands',
+    save: 'Saves the current game and quits to the main menu',
+    quit: 'Quits the current game to the main menu'
+  }
+
   def initialize(min_len=5, max_len=12, max_wrong=8)
     @min_len = min_len
     @max_len = max_len
@@ -13,6 +19,7 @@ class Hangman
 
 
   def main_menu
+    puts ''
     puts 'Welcome to Hangman!'
     puts '1. New game'
     puts '2. Load game'
@@ -30,9 +37,7 @@ class Hangman
     when '1'
       new_game
     when '2'
-      load_menu
-    else
-      return
+      load_game_menu
     end
   end
 
@@ -40,20 +45,25 @@ class Hangman
   def new_game
     @secret = rand_word
     puts 'Let\'s play Hangman!'
-    game_loop
-    display_results
+    
+    if game_loop?
+      # Prematurely exit the game
+    else
+      display_results
+    end
+    main_menu
   end
 
 
-  def load_menu
-    saves = Dir.exist?('saves') ? Dir.entries('saves') - ['..', '.'] : []
+  def load_game_menu
+    saves = Dir.exist?(:saves) ? Dir.entries(:saves) - ['..', '.'] : []
     if saves.empty?
       puts 'No saves found, returning to main menu...'
-      return
-    else
-      saves.each_with_index do |val, index|
-        puts "#{index+1}: #{val}"
-      end
+      return main_menu
+    end
+
+    saves.each_with_index do |val, index|
+      puts "#{index+1}. #{val}"
     end
   end
 
@@ -70,7 +80,7 @@ class Hangman
 
 
   def get_incorrect
-    (@guessed.to_a - @secret.chars).sort
+    (@guessed - @secret.chars).sort
   end
   
 
@@ -95,9 +105,20 @@ class Hangman
   end
 
 
-  def guess_valid?(input)
-    input.length == 1 and input.match(/^[[:alpha:]]+$/)
+  def input_valid?(input)
+    guess_valid?(input) or command_valid?(input)
   end
+
+
+  def command_valid?(command)
+    @@comands.keys.map { |k| k.name }.include? command
+  end
+
+  
+  def guess_valid?(guess)
+    guess.length == 1 and guess.match(/^[[:alpha:]]+$/)
+  end
+
 
   def choice_valid?(input, max_index)
     int = Integer(input) rescue -1
@@ -105,23 +126,33 @@ class Hangman
   end
 
 
-  def game_loop
+  # Returns true if we quit the game before it ended
+  def game_loop?
+    display_commands
     display_game
+
     until game_over?
       print 'Guess a letter: '
-      guess = gets.chomp
-    
-      until guess_valid?(guess)
+      input = gets.chomp
+      
+      until input_valid?(input)
         puts 'Invalid letter!'
         display_game
     
         print 'Guess a letter: '
-        guess = gets.chomp
+        input = gets.chomp
       end
-      @guessed << guess.downcase
+
+      if guess_valid? input
+        @guessed << input.downcase
+      elsif command_valid? input
+        return true if execute_command? input
+      end
       display_game
     end
+    false
   end
+
 
   def display_game
     puts ''
@@ -130,9 +161,40 @@ class Hangman
   end
 
 
+  def display_commands
+    puts ''
+    puts 'Commands:'
+    for k, v in @@comands
+      puts "   #{k} - #{v}"
+    end
+  end
+
+
+  # Returns true when we want to quit the current game
+  def execute_command?(input)
+    case input
+    when 'help'
+      display_commands
+    when 'save'
+      return true
+    when 'quit'
+      return true
+    end
+    false
+  end
+
+
   def game_over?
     incorrect = get_incorrect
     @secret.chars.all?{ |c| @guessed.include? c} or incorrect.length == @max_wrong
+  end
+
+
+  def save_game
+  end
+
+
+  def load_game
   end
 end
 
